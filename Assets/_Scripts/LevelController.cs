36aae23ef043435454e3af09d0c97a036dce5d53
Michael Denkovski <michaeldenkovski@hotmail.com>
2021-02-18 15:57:46 -0500

    Initial commit

---
+++ Assets/_Scripts/LevelController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class LevelController : MonoBehaviour
+{
+    public NavMeshSurface navMeshSurface;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshSurface = GetComponent<NavMeshSurface>();
+        navMeshSurface.BuildNavMesh();
+    }
+
+}
